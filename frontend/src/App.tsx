ca9ba2e588362101998ebfaed2be09b1565a812e
kumarchandan125 <chandanpandey6048178@gmail.com>
2026-06-02 15:06:23 +0530

    feat: initialize React frontend project with Vite, Tailwind CSS, and core page routing structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,58 @@
+import { Toaster } from "react-hot-toast";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductPage from "./pages/ProductPage";
+import SearchResultPage from "./pages/SearchResultPage";
+import FlashDeals from "./pages/FlashDeals";
+import Checkout from "./pages/Checkout";
+import MyOrders from "./pages/MyOrders";
+import OrderTracking from "./pages/OrderTracking";
+import Addresses from "./pages/Addresses";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#1B3022",
+            color: "#fff",
+            fontSize: "14px",
+            padding: "16px 24px",
+            borderRadius: "12px",
+            boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+            border: "1px solid #2D432E",
+          },
+        }}
+      />
+
+      <Routes>
+        {/*Auth pages- no navbar/footer */}
+        <Route path="/login" element={<Login />} />
+        {/*Main app pages- with navbar and footer */}
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<ProductPage />} />
+          <Route path="search" element={<SearchResultPage />} />
+          <Route path="deals" element={<FlashDeals />} />
+
+          <Route element={<ProtectedRoutes />}>
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="orders" element={<MyOrders />} />
+            <Route path="orders/:id" element={<OrderTracking />} />
+            <Route path="addresses" element={<Addresses />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/AppLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <>
+    <p>Banner</p>
+    <p>Navbar</p>
+
+    <main className="min-h-screen">
+      <Outlet/>
+    </main>
+    <p>footer</p>
+    <p>cartsidebar</p>
+    </>
+  );
+};
+
+export default AppLayout;
